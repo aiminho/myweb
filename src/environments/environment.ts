cde5d8a86e7bca18389c8b2ae63c109696399d97
KococoOhYeah <sirichai.srr@gmail.com>
2019-03-29 23:21:10 +0700

    firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
   appname: 'Angular 101',
   firebase: {
-    apiKey: 'AIzaSyC4xE7W6OtoAA3zUxV51pVb_bxRR4nLM9E',
-    authDomain: 'mybase-160420.firebaseapp.com',
-    databaseURL: 'https://mybase-160420.firebaseio.com',
-    projectId: 'mybase-160420',
-    storageBucket: 'mybase-160420.appspot.com',
-    messagingSenderId: '518740224667'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
 
